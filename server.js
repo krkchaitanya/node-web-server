654a0edaef78a374a9386b6c696211707d428cba
krkchaitanya <krkchaitanya42@gmail.com>
2017-01-25 11:08:09 -0600

    Addproject page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about',(req,res)=>{
   });
   });
 
+  app.get("/projects",(req,res)=>{
+    res.render("projects.hbs",{
+      pageTitle:"projects"
+    });
+  });
+
   app.get('/home',(req,res)=>{
     res.render('home.hbs',{
       pageTitle:"help page",
